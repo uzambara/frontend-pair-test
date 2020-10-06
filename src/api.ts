390b99deedeab0bde0c668070864a10f6e142263
Fernando De Freitas <fernando.defreitas@gfk.com>
2020-10-06 18:28:47 +0100

    Add 'api' module

---
+++ src/api.ts
@@ -0,0 +1,40 @@
+
+export async function fetchUpcomingLaunches(limit: number): Promise<any[]> {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    const body = JSON.stringify({
+        query: `{
+            launchesUpcoming(limit: ${limit}) {
+                rocket {
+                    rocket_name
+                    second_stage {
+                        payloads {
+                        payload_type
+                        }
+                        block
+                    }
+                }
+                launch_date_utc
+                mission_name
+                id
+                links {
+                    mission_patch_small
+                }
+            }
+        }`,
+        variables: {}
+    });
+
+    const options: RequestInit = {
+        method: "POST",
+        redirect: "follow",
+        headers,
+        body,
+    };
+
+    const request = await fetch("https://api.spacex.land/graphql/", options);
+    const response = await request.json();
+
+    return response.data.launchesUpcoming;
+}
